7f4e6778a5e664278a0c306aef8030cc5a6b2914
mhkhanju <mahediruman4@gmail.com>
2022-12-22 21:48:49 +0600

    JU_Diary_Accomodations_Office

---
+++ lib/Office.dart
@@ -0,0 +1,364 @@
+final OfficeInfo = {
+  "Vice Chancellor Office": {
+    " Vice Chancellor ": Person(
+        department: " Department of Physics ",
+        designation: " Professor ",
+        email: " alamphys@juniv.edu ",
+        name: " Dr. Md. Nurul Alam ",
+        number: " 01730591462 ",
+        other: "Vice Chancellor"),
+
+    " Secretary to Vice Chancellor ": Person(
+        department: " ",
+        designation: " ",
+        email: " ",
+        name: " Goutam Kumar Biswas ",
+        number: " 01748388767 ",
+        other: " Secretary to Vice Chancellor "),
+
+  }
+
+  "Pro Vice Chancellor (Administration) Office":{
+    " Pro Vice Chancellor (Administration) Office ": Person(
+        department: " Department of Geography & Environment ",
+        designation: " Professor ",
+        email: "monzurulh@juniv.edu",
+        name: " Professor Sheikh Md. Monzurul Huq ",
+        number: " ",
+        other: " Pro Vice Chancellor (Administration) Office "),
+  }
+
+  "Pro Vice Chancellor (Academic) Office":{
+    " Pro Vice Chancellor (Academic) Office": Person(
+        department: "  ",
+        designation: " ",
+        email: " ",
+        name: " ",
+        number: " ",
+        other: " Pro Vice Chancellor (Academic) Office "),
+  }
+
+  " Treasurer Office ":{
+    " ": Person(
+        department: " Department of Anthropology",
+        designation: "Professor ",
+        email: " rashedaakhtar@juniv.edu ",
+        name: "Professor Dr. Rasheda Akhtar ",
+        number: " 8801711160998 ",
+        other: " Treasurer Office "),
+  }
+
+  " Registrar Office ":{
+    " Registrar ": Person(
+        department: "  ",
+        designation: " ",
+        email: "  registrar@juniv.edu ",
+        name: " ",
+        number: "  02224 491040 (Off.) ",
+        other: " Registrar "),
+  }
+
+  " Proctor Office ":{
+    " Proctor (Acting)": Person(
+        department: " Department of Government & Politics ",
+        designation: " Associate Professor ",
+        email: "  firoz_gp@juniv.edu ",
+        name: " A S M Firoz Ul Hasan ",
+        number: " 01750122305 ",
+        other: " Proctor (Acting) "),
+
+    " Administrative Officer ": Person(
+        department: "  ",
+        designation: " ",
+        email: "  sohelju38@gmail.com ",
+        name: " Md. Sohel Rana ",
+        number: " Office Phone: 1263 ",
+        other: " Administrative Officer "),
+
+    " Assistant Registrar ": Person(
+        department: "  ",
+        designation: " ",
+        email: "  goutamju@yahoo.com ",
+        name: " Goutam Kumar Biswas ",
+        number: "Office Phone: 1263 ",
+        other: " Assistant Registrar "),
+  }
+
+  " Comptroller Office ":{
+    " Comptroller ": Person(
+        department: "  ",
+        designation: " ",
+        email: "Md. Mosanul Kabir ",
+        name: " ",
+        number: " ",
+        other: " Comptroller "),
+  }
+
+  " Engineering Office ":{
+    " Chief Engineer (Acting) ": Person(
+        department: "  ",
+        designation: " ",
+        email: "  university.engr.ju@gmail.com  ",
+        name: " Engr. Abdus Salam Md. Sharif ",
+        number: " 01911320486 ",
+        other: " Chief Engineer (Acting) "),
+
+    " Deputy Registrar ": Person(
+        department: "  ",
+        designation: " ",
+        email: " bilkis.begum22j.u@gmail.com ",
+        name: " Bilkis Begum ",
+        number: " Office Phone: 02224491045-51 Ext. 2271 ",
+        other: " Deputy Registrar "),
+
+    "Deputy Chief Engineer (Civil) ": Person(
+        department: "  ",
+        designation: " ",
+        email: " abdulmatin12357@gmail.com ",
+        name: "Md. Abdul Motin ",
+        number: " 01715657999 ",
+        other: "Deputy Chief Engineer (Civil)"),
+
+    " Deputy Chief Engineer (Electric) ": Person(
+        department: "  ",
+        designation: " ",
+        email: " ",
+        name: " Md. Shamsul Haque Mredha ",
+        number: " 01712168601 ",
+        other: " Deputy Chief Engineer (Electric) "),
+
+    "Deputy Chief Engineer (Civil) ": Person(
+        department: "  ",
+        designation: " ",
+        email: " ",
+        name: " Engr. Md. Ahsan Habib ",
+        number: " ",
+        other: " Deputy Chief Engineer (Civil)"),
+
+    " Deputy Chief Engineer (Electric) ": Person(
+        department: "  ",
+        designation: " ",
+        email: " ",
+        name: "Engr. Md. Shariful Islam Bhuiyan ",
+        number: "  01817003211 ",
+        other: " Deputy Chief Engineer (Electric) "),
+
+    " Deputy Chief Engineer (Civil)": Person(
+        department: "  ",
+        designation: " ",
+        email: " ",
+        name: " Engr. Assaduzzaman ",
+        number: " 01716685350 ",
+        other: "Deputy Chief Engineer (Civil) "),
+
+    " Deputy Chief Engineer (Civil) ": Person(
+        department: "  ",
+        designation: " ",
+        email: "engr.ashik@juniv.edu ",
+        name: " Engr. Md. Ashikur Rohman ",
+        number: " 01912200219 ",
+        other: " Deputy Chief Engineer (Civil) "),
+
+    "Assistant Registrar ": Person(
+        department: "  ",
+        designation: " ",
+        email: " faridalome85@gmail.com ",
+        name: "A.K.M. Faridul Alom ",
+        number: " Office Phone: 02224491045-51 Ext. 1218 ",
+        other: " Assistant Registrar"),
+
+    "Senior Technical Officer (works) ": Person(
+        department: "  ",
+        designation: " ",
+        email: " ",
+        name: " Prodip Kumer Biswash ",
+        number: " ",
+        other: "Senior Technical Officer (works) "),
+
+    " Assistant Comptroller (Accounts)": Person(
+        department: "  ",
+        designation: " ",
+        email: " azizrume22@gmail.com ",
+        name: "Abdul Aziz ",
+        number: " 01819485433 ",
+        other: " Assistant Comptroller (Accounts)"),
+
+    " Administrative Officer": Person(
+        department: "  ",
+        designation: " ",
+        email: " abuhanif4@gmail.com ",
+        name: " Muhammad Abu Hanif ",
+        number: " ",
+        other: " Administrative Officer"),
+
+    " Assistant Engineer ( Civil)": Person(
+        department: "  ",
+        designation: " ",
+        email: " ",
+        name: " Anowara Begum ",
+        number: " Office Phone: 02224491045-51 Ext. 1219 ",
+        other: " Assistant Engineer ( Civil)"),
+
+    " Technical Officer ": Person(
+        department: "  ",
+        designation: " ",
+        email: " akazadju10@gmail.com ",
+        name: " Md. Abul Kalam Azad ",
+        number: " ",
+        other: "Technical Officer "),
+
+    "Technical Officer (works) ": Person(
+        department: "  ",
+        designation: " ",
+        email: " khanhannan7441@gmail.com ",
+        name: " Mohammad Abdul Hannan",
+        number: "  01716632049 ",
+        other: " Technical Officer (works) "),
+
+    " Assistant Engineer (Electric) ": Person(
+        department: "  ",
+        designation: " ",
+        email: " hasibul44@gmail.com ",
+        name: " Md. Hasibul Islam ",
+        number: "01671103000 ",
+        other: " Assistant Engineer (Electric) "),
+
+  }
+  " IQAC ":{
+    " Director ": Person(
+        department: " Department of Zoology ",
+        designation: "Professor ",
+        email: "feerozmm@juniv.edu ",
+        name: " Mohammed Mostafa Feeroz ",
+        number: "8801743471731 ",
+        other: "Director "),
+
+    " Additional Director ": Person(
+        department: "  ",
+        designation: " ",
+        email: " tksaha_ju@juniv.edu ",
+        name: "Dr. Topon Kumar Saha ",
+        number: " ",
+        other: " Additional Director "),
+
+    " Additional Director": Person(
+        department: "  ",
+        designation: " ",
+        email: " m.s.rashid@juniv.edu ",
+        name: " Dr. Md. Shahedur Rashid ",
+        number: " ",
+        other: " Additional Director"),
+
+    " Director ": Person(
+        department: "  ",
+        designation: " ",
+        email: " ajit@juniv.edu ",
+        name: " Dr. Ajit Kumar Mazumdar ",
+        number: " ",
+        other: " Director "),
+  }
+
+  " ":{
+    " ": Person(
+        department: "  ",
+        designation: " ",
+        email: " ",
+        name: " ",
+        number: " ",
+        other: " "),
+  }
+
+  "ICT Cell ":{
+    " Teacher in Charge ": Person(
+        department: "  ",
+        designation: " ",
+        email: " ",
+        name: " Dr. Mohammad Shorif Uddin, B.Sc. Engg. (BUET), M.Tech.Ed.(Japan), Ph.D.(Japan) ",
+        number: " ",
+        other: " "),
+  }
+
+  " ":{
+    " ": Person(
+        department: "  ",
+        designation: " ",
+        email: " ",
+        name: " ",
+        number: " ",
+        other: " "),
+  }
+
+  " ":{
+    " ": Person(
+        department: "  ",
+        designation: " ",
+        email: " ",
+        name: " ",
+        number: " ",
+        other: " "),
+  }
+
+  " ":{
+    " ": Person(
+        department: "  ",
+        designation: " ",
+        email: " ",
+        name: " ",
+        number: " ",
+        other: " "),
+  }
+
+  " ":{
+    " ": Person(
+        department: "  ",
+        designation: " ",
+        email: " ",
+        name: " ",
+        number: " ",
+        other: " "),
+  }
+
+  " ":{
+    " ": Person(
+        department: "  ",
+        designation: " ",
+        email: " ",
+        name: " ",
+        number: " ",
+        other: " "),
+  }
+
+  " ":{
+    " ": Person(
+        department: "  ",
+        designation: " ",
+        email: " ",
+        name: " ",
+        number: " ",
+        other: " "),
+  }
+
+
+
+
+
+
+
+
+};
+
+class Person {
+  String name;
+  String designation;
+  String number;
+  String email;
+  String department;
+  String other;
+
+  Person({required this.name,
+    required this.designation,
+    required this.number,
+    required this.email,
+    required this.department,
+    required this.other});
+}
